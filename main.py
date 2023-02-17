9c6b772f8d3137d86183947c0ba03f68382c835a
alfre <alfredo.medina@uc.cl>
2023-02-17 16:40:08 -0300

    to do: fix removing jobs of job queue

--- main.py
+++ main.py
@@ -1,27 +1,83 @@
+
+import logging
 from os import environ
 from dotenv import load_dotenv
-import telebot
 
-load_dotenv(".env")
+from telegram import __version__ as TG_VER
+from telegram import __version_info__
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, CallbackContext, JobQueue, Updater
 
-API_KEY = environ['API_KEY']
-
-bot = telebot.TeleBot(API_KEY)
-
-@bot.message_handler(commands=['hello'])
-def hello(message):
-    bot.reply_to(message, "Hello!")
-
-
-@bot.message_handler(commands=['start', 'song'])
-def start(message):
-    bot.send_message(message.chat.id, "Hello!, I'm Miku Hatsune, I'm a bot that sends you a random song from my library, just type /song and I'll send you one!")
-def song(message):
-    bot.send_message(message.chat.id, "Here's a song for you!")
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 
-def get_song_from_youtube():
-    pass
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Sends explanation on how to use the bot."""
+    await update.message.reply_text("Hi! Use /set to set a timer")
 
 
-bot.polling()
+
+def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
+
+
+async def callback_minute(context: CallbackContext):
+    job = context.job
+    await context.bot.send_message(chat_id=job.chat_id,
+                             text='The song of the day')
+
+
+async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Add a job to the queue."""
+    chat_id = update.effective_message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    context.job_queue.run_repeating(callback_minute, chat_id=chat_id, interval=5, first=1)
+    # context.job_queue.run_daily(callback_auto_message, time=datetime.time(hour=9, minute=22), days=(0, 1, 2, 3, 4, 5, 6), context=chat_id)
+    text = "Timer successfully set!"
+    if job_removed:
+        text += " Old one was removed."
+    await update.effective_message.reply_text(text)
+
+async def unset(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = "Timer successfully cancelled!" if job_removed else "You have no active timer."
+    await update.message.reply_text(text)
+
+
+def main() -> None:
+    """Start the bot."""
+    load_dotenv(".env")
+    API_KEY = environ['API_KEY']
+    application = Application.builder().token(API_KEY).build()
+
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler(["start", "help"], start))
+    application.add_handler(CommandHandler("set", set_timer))
+    application.add_handler(CommandHandler("unset", unset))
+
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
